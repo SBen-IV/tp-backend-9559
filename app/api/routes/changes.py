7a7486347e7ff782849d4ec0f0a352edd98a366a
LucianaH <56140002+LucianaH@users.noreply.github.com>
2025-10-04 19:49:30 -0300

    refactor(get-changes): remove duplicate function

--- app/api/routes/changes.py
+++ app/api/routes/changes.py
@@ -20,20 +20,6 @@ async def create_change(
 
     return cambio
 
-@router.get("/", response_model=list[CambioPublicoConItems])
-async def get_changes(
-    session: SessionDep,
-    titulo: str | None = None,
-    prioridad: Prioridad | None = None,
-    estado: EstadoCambio | None = None,
-    descripcion: str | None = None
-) -> list[CambioPublicoConItems]:
-    cambio_filter = CambioFilter(
-        titulo=titulo, estado=estado, prioridad=prioridad, descripcion=descripcion
-    )
-    return crud.get_changes(
-        session=session, cambio_filter=cambio_filter
-    )
     
 @router.get("/", response_model=list[CambioPublicoConItems])
 async def get_changes(
@@ -51,7 +37,7 @@ async def get_changes(
     )
     
 @router.get("/{id_change}", response_model=CambioPublicoConItems)
-async def get_config_item(
+async def get_change(
     session: SessionDep, id_change: uuid.UUID
 ) -> CambioPublicoConItems:
     return crud.get_change_by_id(
