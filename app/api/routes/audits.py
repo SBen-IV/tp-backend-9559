ff85fcfb954fa572f91f0e25af76e9d8c7107b3e
Luciana Hoszowski <lhoszowski@fi.uba.ar>
2025-10-16 16:46:07 -0300

    refactor: rename enunm Accion for Operacion

--- app/api/routes/audits.py
+++ app/api/routes/audits.py
@@ -6,7 +6,6 @@ from app.api.deps import CurrentUser, SessionDep
 from app.crud.audits import AuditoriaService as crud
 from app.models.auditoria import Auditoria, AuditoriaCrear
 from app.crud.audits import AuditoriaService
-from app.models.commons import TipoEntidad, Accion
 
 router = APIRouter(prefix="/audits")
 
