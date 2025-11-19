222ca4e7958f6ba2937d3ec47afd9790155d46d1
LucianaH <lhoszowski@fi.uba.ar>
2025-11-19 11:53:17 -0300

    fix: rename column in CambioIncidenteLink

--- app/models/changes_incidents_link.py
+++ app/models/changes_incidents_link.py
@@ -5,4 +5,4 @@ class CambioIncidenteLink(SQLModel, table=True):
     __tablename__: str = "cambio_incidente_link"
     
     id_cambio: uuid.UUID = Field(default=None, foreign_key="cambios.id", primary_key=True)
-    id_incidentes: uuid.UUID = Field(default=None, foreign_key="incidentes.id", primary_key=True)
+    id_incidente: uuid.UUID = Field(default=None, foreign_key="incidentes.id", primary_key=True)
