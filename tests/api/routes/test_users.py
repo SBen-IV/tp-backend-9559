144632d23a856ba8073264b0035f06219f654c9b
SBen-IV <72094271+SBen-IV@users.noreply.github.com>
2025-12-07 12:36:22 -0300

    test(user): me

--- tests/api/routes/test_users.py
+++ tests/api/routes/test_users.py
@@ -224,3 +224,23 @@ def test_create_user_validate_password_too_long(
     assert details
     assert details["message"] == "String should have at most 40 characters"
     assert details["field"] == "contraseña"
+
+
+def test_get_users_me(
+    client: TestClient, session: Session, empleado_token_headers: dict[str, str]
+) -> None:
+    # Get the user from token headers
+    usuario = session.exec(
+        select(Usuario).where(Usuario.email == "alice@company.com")
+    ).first()
+
+    r = client.get(f"{BASE_URL}/me", headers=empleado_token_headers)
+
+    assert 200 <= r.status_code < 300
+    user_data = r.json()
+
+    assert user_data["nombre"] == usuario.nombre
+    assert user_data["apellido"] == usuario.apellido
+    assert user_data["email"] == usuario.email
+    assert user_data["rol"] == usuario.rol
+    assert user_data["id"] == str(usuario.id)
